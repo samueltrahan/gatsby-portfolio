4cd90028a5583440e2a601377de103c33c7f630a
Samuel Trahan <samtrahan91@gmail.com>
2020-08-04 07:32:37 -0500

    contact link added

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function contact() {
+  return (
+    <div>
+      Contact Page
+    </div>
+  )
+}
+
+
+
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import { Link } from 'gatsby'
+
+export default function Home() {
+  return (
+  <div>
+    <h2>Hi there!</h2>
+    <p>I'm Samuel, a Austin-based software engineer</p>
+    <Link to="/contact">Contact Me</Link>
+  </div>
+  )
+}
